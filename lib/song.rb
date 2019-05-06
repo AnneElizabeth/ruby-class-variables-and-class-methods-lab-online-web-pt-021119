d02e6d4406c087cdd931505fb9eb5373be653b5d
Anne Edmondson <edmondson.anne@gmail.com>
2019-05-06 04:07:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,13 +3,26 @@ class Song
 
   @@count = 0
 
+  @@genres = []
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
 
-    @@count
+    @@count +=1
+    @@genres << self.genre
 
   end
 
+  def self.count
+    @@count.count
+  end
+  
+  def self.genres
+    genres = Hash.new
+
+    words.each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
+
+  end
 end
